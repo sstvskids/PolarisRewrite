f69dcfcf770e65abe62f191d9515aa4f70578a29
sstvskids <staavstudios@gmail.com>
2025-04-05 10:10:34 +1100

    Update universal.lua

--- universal.lua
+++ universal.lua
@@ -1,1 +1,49 @@
+if not isfolder('polaris') then return nil end
+
+local library: table = loadfile('polaris/libraries/interface.lua')()
+local utils: table = loadfile('polaris/libraries/utils.lua')()
+local connections: table = {}
+
+local cloneref = cloneref or function(v) return v end
+local Players: Players = cloneref(game:GetService('Players'))
+local Lighting: Lighting = cloneref(game:GetService("Lighting"))
+local ReplicatedStorage: ReplicatedStorage = cloneref(game:GetService('ReplicatedStorage'))
+local UserInputService: UserInputService = cloneref(game:GetService('UserInputService'))
+local RunService: RunService = cloneref(game:GetService('RunService'))
+local TweenService: TweenService = cloneref(game:GetService('TweenService'))
+local CollectionService: CollectionService = cloneref(game:GetService("CollectionService"))
+local lplr: Players = Players.LocalPlayer
+
+local user: string = lplr.Name
+local HurtTime: string = 0
+local release: string = 'rewrite'
+
+repeat task.wait() until game:IsLoaded() and lplr.Character
+
+if not isfile("polaris/configs/"..game.PlaceId..".json") then library.saveConfig() end
+library.loadConfig()
+
+lplr.CharacterAdded:Connect(function(char)
+    repeat task.wait(1) until char ~= nil
+	local Character = char
+	local Humanoid = char.Humanoid
+	local PrimaryPart = char.PrimaryPart
+	local Camera = workspace.Camera
+	local CurrentCamera = workspace.CurrentCamera
+
+	Character.Archivable = true
+
+	for i,v in next, connections do
+		task.spawn(function() v(char) end)
+	end
+end)
+
+Combat = library.NewWindow('Combat')
+Player = library.NewWindow('Player')
+Motion = library.NewWindow('Motion')
+Visuals = library.NewWindow('Visuals')
+Misc = library.NewWindow('Misc')
+Exploit = library.NewWindow('Exploit')
+Legit = library.NewWindow('Legit')
+
 return game:GetService('Players').LocalPlayer:Kick('universal.lua == not finished')
