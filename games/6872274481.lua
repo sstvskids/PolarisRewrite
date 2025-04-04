aac5f47a2eb2df405531997bfc0997b697cf4ef0
sstvskids <staavstudios@gmail.com>
2025-04-05 10:49:49 +1100

    finished aura

--- games/6872274481.lua
+++ games/6872274481.lua
@@ -119,6 +119,82 @@ local viewmodel = workspace.Camera.Viewmodel.RightHand.RightWrist
 local weld = viewmodel.C0
 local oldweld = viewmodel.C0
 
+local animRunning: boolean = true
+local auraAnimations: table = {
+	["Smooth"] = {
+		{CFrame = CFrame.new(0.69, -0.7, 0.1) * CFrame.Angles(math.rad(-65), math.rad(55), math.rad(-51)), Timer = 0.1},
+		{CFrame = CFrame.new(0.16, -1.16, 0.5) * CFrame.Angles(math.rad(-179), math.rad(54), math.rad(33)), Timer = 0.16},
+	},
+	["Spin"] = {
+		{CFrame = CFrame.new(0.69, -0.7, 0.1) * CFrame.Angles(math.rad(-90), math.rad(0), math.rad(0)), Timer = 0.05},
+		{CFrame = CFrame.new(0.69, -0.7, 0.1) * CFrame.Angles(math.rad(-145), math.rad(0), math.rad(0)), Timer = 0.05},
+		{CFrame = CFrame.new(0.16, -1.16, 0.5) * CFrame.Angles(math.rad(-180), math.rad(0), math.rad(0)), Timer = 0.05},
+		{CFrame = CFrame.new(0.16, -1.16, 0.5) * CFrame.Angles(math.rad(-220), math.rad(0), math.rad(0)), Timer = 0.05},
+		{CFrame = CFrame.new(0.16, -1.16, 0.5) * CFrame.Angles(math.rad(-270), math.rad(0), math.rad(0)), Timer = 0.05},
+		{CFrame = CFrame.new(0.16, -1.16, 0.5) * CFrame.Angles(math.rad(-310), math.rad(0), math.rad(0)), Timer = 0.05},
+		{CFrame = CFrame.new(0.16, -1.16, 0.5) * CFrame.Angles(math.rad(-360), math.rad(0), math.rad(0)), Timer = 0.05},
+	},
+	["Reverse Spin"] = {
+		{CFrame = CFrame.new(0.69, -0.7, 0.1) * CFrame.Angles(math.rad(90), math.rad(0), math.rad(0)), Timer = 0.05},
+		{CFrame = CFrame.new(0.69, -0.7, 0.1) * CFrame.Angles(math.rad(145), math.rad(0), math.rad(0)), Timer = 0.05},
+		{CFrame = CFrame.new(0.16, -1.16, 0.5) * CFrame.Angles(math.rad(180), math.rad(0), math.rad(0)), Timer = 0.05},
+		{CFrame = CFrame.new(0.16, -1.16, 0.5) * CFrame.Angles(math.rad(220), math.rad(0), math.rad(0)), Timer = 0.05},
+		{CFrame = CFrame.new(0.16, -1.16, 0.5) * CFrame.Angles(math.rad(270), math.rad(0), math.rad(0)), Timer = 0.05},
+		{CFrame = CFrame.new(0.16, -1.16, 0.5) * CFrame.Angles(math.rad(310), math.rad(0), math.rad(0)), Timer = 0.05},
+		{CFrame = CFrame.new(0.16, -1.16, 0.5) * CFrame.Angles(math.rad(360), math.rad(0), math.rad(0)), Timer = 0.05},
+	},
+	["Swoosh"] = {
+		{CFrame = CFrame.new(0.69, -0.7, 0.1) * CFrame.Angles(math.rad(-65), math.rad(55), math.rad(-51)), Timer = 0.1},
+		{CFrame = CFrame.new(0.16, -1.16, 0.5) * CFrame.Angles(math.rad(-179), math.rad(94), math.rad(33)), Timer = 0.16},
+	},
+	["Swang"] = {
+		{CFrame = CFrame.new(0.69, -0.7, 0.1) * CFrame.Angles(math.rad(-65), math.rad(55), math.rad(-51)), Timer = 0.1},
+		{CFrame = CFrame.new(0.16, -1.16, 0.5) * CFrame.Angles(math.rad(-199), math.rad(74), math.rad(43)), Timer = 0.16},
+	},
+	["Zoom"] = {
+		{CFrame = CFrame.new(0.69, -2, 0.1) * CFrame.Angles(math.rad(-65), math.rad(55), math.rad(-51)), Timer = 0.1},
+		{CFrame = CFrame.new(0.16, -0.1, -1) * CFrame.Angles(math.rad(-179), math.rad(94), math.rad(33)), Timer = 0.16},
+	},
+	["Classic"] = {
+		{CFrame = CFrame.new(0.69, -1, 0.1) * CFrame.Angles(math.rad(-16), math.rad(12), math.rad(-21)), Timer = 0.1},
+		{CFrame = CFrame.new(0.69, -2, 0.1) * CFrame.Angles(math.rad(-72), math.rad(21), math.rad(-35)), Timer = 0.07},
+		{CFrame = CFrame.new(0.69, -0.6, 0.1) * CFrame.Angles(math.rad(0), math.rad(0), math.rad(0)), Timer = 0.07},
+	},
+	["Other Spin"] = {
+		{CFrame = CFrame.new(0.69, -2, 0.1) * CFrame.Angles(math.rad(0), math.rad(0), math.rad(90)), Timer = 0.1},
+		{CFrame = CFrame.new(0.16, -0.1, -1) * CFrame.Angles(math.rad(0), math.rad(0), math.rad(180)), Timer = 0.1},
+		{CFrame = CFrame.new(0.16, -0.1, -1) * CFrame.Angles(math.rad(0), math.rad(0), math.rad(270)), Timer = 0.1},
+		{CFrame = CFrame.new(0.16, -0.1, -1) * CFrame.Angles(math.rad(0), math.rad(0), math.rad(360)), Timer = 0.1},
+	},
+	["Corrupt"] = {
+		{CFrame = CFrame.new(0.69, -2, 0.1) * CFrame.Angles(math.rad(0), math.rad(0), math.rad(90)), Timer = 0.1},
+		{CFrame = CFrame.new(0.69, -2, 0.1) * CFrame.Angles(math.rad(0), math.rad(0), math.rad(60)), Timer = 0.3},
+	},
+	["OldAstralAnim"] = {
+		{CFrame = CFrame.new(1, -1, 2) * CFrame.Angles(math.rad(295), math.rad(55), math.rad(290)), Time = 0.2},
+		{CFrame = CFrame.new(-1, 1, -2.2) * CFrame.Angles(math.rad(200), math.rad(60), math.rad(1)), Time = 0.2}
+	},
+	["Test"] = {
+		{CFrame = CFrame.new(1, -1, 2) * CFrame.Angles(math.rad(295), math.rad(55), math.rad(220)), Time = 0.1},
+		{CFrame = CFrame.new(-1, 1, -2.2) * CFrame.Angles(math.rad(200), math.rad(60), math.rad(1)), Time = 0.1},
+		{CFrame = CFrame.new(0.5, -0.5, 1) * CFrame.Angles(math.rad(250), math.rad(40), math.rad(180)), Time = 0.1},
+		{CFrame = CFrame.new(-0.5, 0.5, -1.1) * CFrame.Angles(math.rad(160), math.rad(45), math.rad(5)), Time = 0.1},
+		{CFrame = CFrame.new(0, 0, 0) * CFrame.Angles(math.rad(0), math.rad(0), math.rad(0)), Time = 0.1}
+	},
+}
+local funAnimations: table = {
+	PLAYER_VACUUM_SUCK = "rbxassetid://9671620809",
+	WINTER_BOSS_SPAWN = "rbxassetid://11843861791",
+	GLUE_TRAP_FLYING = "rbxassetid://11466075174",
+	VOID_DRAGON_TRANSFORM = "rbxassetid://10967424821",
+	SIT_ON_DODO_BIRD = "http://www.roblox.com/asset/?id=2506281703",
+	DODO_BIRD_FALL = "rbxassetid://7617326953",
+	SWORD_SWING = "rbxassetid://7234367412",
+}
+
+local animAuraTab: table = {}
+for i,v in pairs(auraAnimations) do table.insert(animAuraTab, i) end
+local targetInfo = Instance.new("TextLabel", lplr.PlayerGui)
 table.insert(connections, Aura)
 Aura = Combat.NewButton({
     Name = "Aura",
@@ -133,28 +209,105 @@ Aura = Combat.NewButton({
                         local selfPrimaryPartPosition = lplr.Character.PrimaryPart.Position
                         local weapon = getBestWeapon()
                         spoofHand(weapon.Name)
+
+                        task.spawn(function()
+                            remotes.SwordHit:FireServer({
+                                chargedAttack = {
+                                    chargeRatio = 0
+                                },
+                                entityInstance = nearestCharacter,
+                                validate = {
+                                    raycast = {
+                                        cameraPosition = workspace.CurrentCamera,
+                                        cursorDirection = CFrame.LookVector
+                                    },
+                                    targetPosition = {
+                                        value = nearestPrimaryPartPosition
+                                    },
+                                    selfPosition = {
+                                        value = selfPrimaryPartPosition
+                                    },
+                                },
+                                weapon = weapon
+                            })
+                        end)
                     end
 
                     task.spawn(function()
-                        remotes.SwordHit:FireServer({
-                            chargedAttack = {
-                                chargeRatio = 0
-                            },
-                            entityInstance = nearestCharacter,
-                            validate = {
-                                raycast = {
-                                    cameraPosition = workspace.CurrentCamera,
-                                    cursorDirection = CFrame.LookVector
-                                },
-                                targetPosition = {
-                                    value = nearestPrimaryPartPosition
-                                },
-                                selfPosition = {
-                                    value = selfPrimaryPartPosition
-                                },
-                            },
-                            weapon = weapon
-                        })
+                        repeat task.wait()
+                            if nearest ~= nil then
+                                pcall(function()
+                                    local animation = auraAnimations[auraAnimation.Option]
+                                    local allTime = 0
+                                    task.spawn(function()
+                                        if CustomAnimation.Enabled then
+                                            animRunning = true
+                                            for i,v in pairs(animation) do allTime += v.Timer end
+                                            for i,v in pairs(animation) do
+                                                local tween = TweenService:Create(viewmodel,TweenInfo.new(v.Timer),{C0 = oldweld * v.CFrame})
+                                                tween:Play()
+                                                task.wait(v.Timer - 0)
+                                            end
+                                            animRunning = false
+                                            game.TweenService:Create(viewmodel,TweenInfo.new(1),{C0 = oldweld}):Play()
+                                        end
+                                    end)
+                                end)
+                            end
+                        until (not Aura.Enabled)
+                    end)
+
+                    task.spawn(function()
+                        repeat task.wait()
+                            if nearest ~= nil then
+                                local isWinning = function() return nearest.Character.Humanoid.Health > lplr.Character.Humanoid.Health end
+                                if targetInfo ~= nil then
+                                    targetInfo = Instance.new('TextLabel', lplr.PlayerGui)
+                                end
+
+                                if TargetHudMode.Option == "Basic" then
+                                    pcall(function()
+                                        targetInfo.Size = UDim2.fromScale(.12, .05)
+                                        targetInfo.BackgroundColor3 = Color3.fromRGB(25,25,25)
+                                        targetInfo.BorderSizePixel = 0
+                                        targetInfo.AnchorPoint = Vector2.new(0.5,0.5)
+                                        targetInfo.Position = UDim2.fromScale(0.6,0.5)
+                                        targetInfo.TextColor3 = Color3.fromRGB(255,255,255)
+                                        targetInfo.Text = "  "..nearest.DisplayName.. " - IsWinning: ".. tostring(isWinning())
+                                        targetInfo.TextXAlignment = Enum.TextXAlignment.Left
+        
+                                        local hp = Instance.new("Frame", targetInfo)
+                                        hp.Position = UDim2.fromScale(0, .9)
+                                        hp.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
+                                        hp.BorderSizePixel = 0
+        
+                                        TweenService:Create(hp,TweenInfo.new(1),{
+                                            Size = UDim2.fromScale(0.01 * nearest.Character.Humanoid.Health,0.1)
+                                        }):Play()
+                                    end)
+                                elseif TargetHudMode.Option == "Basic2" then
+                                    pcall(function()
+                                        TweenService:Create(targetInfo,TweenInfo.new(1),{
+                                            Size = UDim2.fromScale(0.001 * nearest.Character.Humanoid.Health,0.04)
+                                        }):Play()
+                                        targetInfo.BackgroundColor3 = library.Color
+                                        targetInfo.BorderSizePixel = 0
+                                        targetInfo.AnchorPoint = Vector2.new(0.5,0.5)
+                                        targetInfo.Position = UDim2.fromScale(0.6,0.5)
+                                        targetInfo.TextColor3 = Color3.fromRGB(255,255,255)
+                                        targetInfo.Text = "  "..nearest.DisplayName
+                                        --targetInfo.TextScaled = true
+                                        targetInfo.TextXAlignment = Enum.TextXAlignment.Left
+                                    end)	
+                                end
+                            else
+                                pcall(function()
+                                    targetInfo:Remove()
+                                    targetInfo = nil
+                                end)
+                            end
+                            task.wait()
+                        until (not Aura.Enabled)
                     end)
                 end)
             end)
@@ -165,3 +318,18 @@ Aura = Combat.NewButton({
         end
     end
 })
+auraAnimation = Aura.NewPicker({
+	Name = "Animations",
+	Options = animAuraTab
+})
+CustomAnimation = Aura.NewToggle({
+	Name = "CustomAnimation"
+})
+TargetHudMode = Aura.NewPicker({
+	Name = "TargetHud",
+	Options = {"None", "Basic", "Basic2"}
+})
+
+table.insert(connections, function(char)
+	viewmodel = workspace.Camera.Viewmodel.RightHand.RightWrist
+end)
